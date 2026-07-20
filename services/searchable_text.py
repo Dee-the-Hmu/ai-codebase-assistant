ef7c6868c440e7a92f96733ab9a698c66e3f0ed5
Dee <aeinhmu.naychi@sjsu.edu>
2026-07-19 23:43:54 -0700

    to add metadata to code chunk before converting into embeddings

---
+++ services/searchable_text.py
@@ -0,0 +1,25 @@
+
+
+def build_searchable_text(
+        repo_name : str,
+        file_path : str,
+        class_name : str | None,
+        func_name : str | None,
+        text_content : str
+) -> str: 
+    
+    words_list = [
+        f"Repository: {repo_name}",
+        f"File: {file_path}",
+    ]
+
+    if class_name is not None: 
+        words_list.append(f"Class: {class_name}")
+    if func_name is not None:
+        words_list.append(f"Function: {func_name}")
+
+    words_list.append(f"Code:\n{text_content}")
+
+    searchable_text = "\n".join(words_list)
+
+    return searchable_text
