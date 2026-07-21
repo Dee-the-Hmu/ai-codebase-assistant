70cc9b74b9c0b583163126dc1beb95cb01fd463e
Dee <aeinhmu.naychi@sjsu.edu>
2026-07-20 19:45:41 -0700

    Iterate the repo_root and return the paths to all files under the repo_root

---
+++ services/file_processing/discover_file_paths.py
@@ -0,0 +1,15 @@
+"""
+given a path to the repo_root (top lvl folder) of the extracted folder
+    iterate the files and 
+    return a list of the paths to all files
+"""
+from pathlib import Path 
+def get_repo_files(repo_root : Path) -> list[Path]:
+
+    files_paths = []
+
+    for path in repo_root.rglob("*"): #rglob("*") = recursively finds everything inside it, including nested files and folders
+        if path.is_file():
+            files_paths.append(path)
+        
+    return files_paths
