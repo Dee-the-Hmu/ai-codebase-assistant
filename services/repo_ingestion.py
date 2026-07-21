43aea33e1730e890d680936026d40f2a75a94552
Dee <aeinhmu.naychi@sjsu.edu>
2026-07-20 21:34:34 -0700

    Implements part of ingest_files that will add File to the files table in the database

--- services/repo_ingestion.py
+++ services/repo_ingestion.py
@@ -27,10 +27,19 @@ Use the downloaded files (extracted repository)
 """
 
 from sqlalchemy.orm import Session
+
 from models.repository import Repository
 from crud.repository import read_repo_with_github_url, create_repo
 from github.url_validation import validate_and_parse_github_url
 from github.client import get_repository, check_public, make_RepositoryCreate_Obj
+
+from file_processing.discover_file_paths import get_all_repo_files
+from file_processing.file_filtering import filter_supported_file_paths
+from file_processing.read_file import get_text_content
+from models.file import File
+from schemas.file import FileCreate
+from crud.file import create_file, read_file_with_path_and_repo_id
+from pathlib import Path
 
 def ingest_repository(db : Session, github_url : str)-> Repository | None: 
 
@@ -58,8 +67,54 @@ def ingest_repository(db : Session, github_url : str)-> Repository | None:
     #create a RepositoryCreate object ready to insert to the db
     repo_create_pydantic_obj = make_RepositoryCreate_Obj(owner=owner, repo_name=repo_name, repo_response_dict=repo_response_dict)
 
-
     #insert that RepositoryCreate object using curd's create_repo into the database
     repository_obj = create_repo(db, repo_data=repo_create_pydantic_obj)
 
     return repository_obj
+
+def ingest_files(db : Session, repo_root : Path, repository_obj : Repository) -> list[Path]:
+    #discover all file paths
+    list_of_all_file_paths = get_all_repo_files(repo_root=repo_root)
+
+    #filter only the supported files' paths
+    list_of_all_supported_file_paths = filter_supported_file_paths(list_of_all_file_paths)
+
+    for file_path in list_of_all_supported_file_paths: 
+
+        #read the text_content 
+        text_content = get_text_content(file_path=file_path)
+        
+        #skips unreadable or empty files 
+        if text_content is None or not text_content.strip():
+            continue #dont create the File
+
+
+        #creates the file_path to make it a relative path -> to store useful relative file_paths inside the files
+        relative_file_path = file_path.relative_to(repo_root)
+        
+        #turns it into str for database
+        file_path_str = str(relative_file_path)
+
+        #check if the file already exist in the files table 
+        existing_file = read_file_with_path_and_repo_id(db, file_path=file_path_str, repo_id=repository_obj.id)
+
+        if existing_file: #if file already inside the table, don't add it again
+            continue
+
+
+        #else, create a FileCreate Pydantic Object ready to insert to the db
+        file_create_pydantic_obj = FileCreate(
+            repo_id=repository_obj.id,
+            path=file_path_str,
+            name=file_path.name,
+            size_bytes=file_path.stat().st_size
+        )
+
+        #insert that FileCreate object using curd's create_file into the the database
+        file_obj = create_file(db, file_data=file_create_pydantic_obj)
+
+        #chunk here (continue ingesting)
+        ...
+
+       
+
