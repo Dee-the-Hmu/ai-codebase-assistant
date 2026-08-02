41a846e31baac5dba42306b65b573756156ea784
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-02 00:45:54 -0700

    Make health.py looks pretty

--- routers/health.py
+++ routers/health.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 
-rounter = APIRouter(prefix="/health")
+router = APIRouter()
+
+"""
+1. You can test that your server is alive.
+2. Docker/AWS can use it to check whether your app is working.
+3. It is useful in interviews because it shows production thinking.
+"""
 
 @router.get("/health")
 def health_check():
-    """
-    1. You can test that your server is alive.
-    2. Docker/AWS can use it to check whether your app is working.
-    3. It is useful in interviews because it shows production thinking.
-    """
     return{"status" : "ok"}
