fbd80e1a35ba01f7f1b0c9b2730af5c82b56ccdc
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-03 01:42:50 -0700

    rename the existing main to fastapi_main and create flask_main that will use Flask for web dev

---
+++ fastapi_main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI 
+from routers.fastapi.repositories import router as repositories_router
+from routers.fastapi.questions import router as questions_router
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="AI Codebase Assistant",
+    verion="1.0.0"
+)
+
+"""
+Browsers block frontend requests to a different origin unless the backend explicitly permits them.
+"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(repositories_router)
+app.include_router(questions_router)
