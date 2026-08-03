9fd474e4f87057da2a8c085d991f5d9e98a37eaa
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-02 20:08:43 -0700

    adds title

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from routers.repositories import router as repositories_router
 from routers.questions import router as questions_router
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Codebase Assistant",
+    verion="1.0.0"
+)
 
 app.include_router(repositories_router)
 app.include_router(questions_router)
