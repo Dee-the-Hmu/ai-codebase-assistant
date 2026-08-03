6d50ae7e33d899def5d9b90cf8a82db6180cf1c6
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-02 19:13:10 -0700

    Add hidden api key

--- schemas/config.py
+++ schemas/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str
     github_token: str
+    openai_api_key : str
 
     model_config = SettingsConfigDict(
         env_file=".env"
