e48bbf1fbbf57c1fbe4587711733733795caeb1d
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-02 19:11:19 -0700

    Test the questions router

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI 
 from routers.repositories import router as repositories_router
+from routers.questions import router as questions_router
 
 app = FastAPI()
 
 app.include_router(repositories_router)
+app.include_router(questions_router)
