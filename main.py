186e4782686f99e3a9c53ed974a8f0248cb76498
Dee <aeinhmu.naychi@sjsu.edu>
2026-08-02 22:12:40 -0700

    Adds CORS that let the frontend connects to the backend by sending an HTTP request

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from fastapi import FastAPI
 from routers.repositories import router as repositories_router
 from routers.questions import router as questions_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(
     title="AI Codebase Assistant",
     verion="1.0.0"
+)
+
+"""
+Browsers block frontend requests to a different origin unless the backend explicitly permits them.
+"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(repositories_router)
